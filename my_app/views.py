e811e8b50c91db06772fedd677b21c202b020c3a
Rushil <rushil1904@gmail.com>
2021-05-24 20:55:51 +0530

    Changes in submit button and csrf token

--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    return render(request, 'my_app/new_search.html')

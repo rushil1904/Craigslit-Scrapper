e8734c75fcf357521085d7b2314b69dae1b17758
Rushil <rushil1904@gmail.com>
2021-05-24 23:35:47 +0530

    Added workign search and connection with model

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,11 @@
+from django.http import response
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from . import models
+from requests.compat import quote_plus
+
+BASE_CRAIGSLIST_URL = "https://delhi.craigslist.org/search/?query={}"
 
 # Create your views here.
 def home(request):
@@ -6,6 +13,11 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    print(data)
     final_postings = []
     stuff_for_frontend = {
         'search': search,
